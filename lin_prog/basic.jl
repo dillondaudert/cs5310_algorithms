328849aa27a57fe2ee0d6fc886c562ec741880bd
Dillon Daudert <dillongdaudert@gmail.com>
2017-12-03 13:57:29 -0500

    Add auxlp function to create auxiliary linear program for initsimplex. Add test case

--- lin_prog/basic.jl
+++ lin_prog/basic.jl
@@ -16,3 +16,16 @@ function expandlp(A::AbstractArray, b::AbstractArray, c::AbstractArray)
     c′[1:n] = c
     return (A′, b′, c′)
 end
+
+"""
+Form the auxiliary linear program Lₐ by adding the nonbasic variable x₀.
+Set the objective function to -x₀
+Return (Lₐ, b, cₐ)
+"""
+function auxlp(A::AbstractArray, b::AbstractArray, c::AbstractArray)
+    Lₐ = fill(-1.0, (size(A, 1), size(A, 2) + 1))
+    Lₐ[:, 2:size(Lₐ, 2)] = A
+    cₐ = fill(0.0, (1, length(c)+1))
+    cₐ[1] = -1
+    return (Lₐ, b, cₐ)
+end
--- lin_prog/test/basic_test.jl
+++ lin_prog/test/basic_test.jl
@@ -21,12 +21,24 @@ trueA′ = [-1/6 -1/6 1/3 0 0 0;
 truec′ = [-1/6 -1/6 -2/3 0 0 0]
 trueb′ = [8. 4. 18. 0. 0. 0.]
 
+trueLₐ = [-1. -1/6 -1/6 1/3;
+     -1. 8/3 2/3 -1/3;
+     -1. 1/2 -1/2 0;]
+truecₐ = [-1. 0. 0. 0.]
+
 @testset "All Tests" begin
     @testset "Expand Tests" begin
         (A′, b′, c′) = expandlp(A, b, c)
         @test A′ == trueA′
         @test b′ == trueb′
         @test c′ == truec′
+    end
+
+    @testset "Auxiliary Tests" begin
+        Lₐ, bₐ, cₐ = auxlp(A, b, c)
+        @test Lₐ == trueLₐ
+        @test bₐ == b
+        @test cₐ == truecₐ
     end
 
 end
